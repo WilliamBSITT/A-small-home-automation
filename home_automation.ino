272167e833d22765af145a3e54cf4776437e5c71
WilliamBSITT <william.sitt@epitech.eu>
2024-08-03 22:31:08 +0200

    <+> ADD now the ligh can be trigered on and off by the arduin (ps : I hate LCD)

--- home_automation.ino
+++ home_automation.ino
@@ -1,13 +1,37 @@
+#include <LiquidCrystal.h>
+
 const int lumos = 7;
+const int photoresistor = A0;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16,2);
   pinMode(lumos, OUTPUT);
   digitalWrite(lumos, LOW);
 }
 
+void lumos_automata() {
+  int val = analogRead(photoresistor);
 
+  //Serial.println(val); // Can help to debug if you try to find the right value
+  if (val <= 80) { // This value can change du to the room, where you place the arduino ...
+    //Serial.println("Status lamp :OFF");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Status lamp :OFF");
+    digitalWrite(lumos, LOW);
+  } else {
+    //Serial.println("Status lamp :ON");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Status lamp :ON");
+    digitalWrite(lumos, HIGH);
+  }
+}
 
 void loop() {
+  lumos_automata()
   delay(1000);
 }
